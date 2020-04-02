89ae60ae3c63f8f15f4a1f737ff6adf87afbf786
KAY S <KS.KWANRUAN@GMAIL.com>
2020-04-03 01:42:17 +1100

    promises chaining

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
+//This is a FAKE Http Request Function
+//It takes 1 second to resolve or reject the promise, depending on the url that is passed in
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const pages = {
         "/users": [
-          { id: 1, users: `Bilbo` },
-          { id: 5, username: `Esmeralda` }
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" }
         ],
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu"
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet"
+        },
         "/about": "This is the about page!"
       };
       const data = pages[url];
       if (data) {
-        resolve({ status: 200, data });
+        resolve({ status: 200, data }); //resolve with a value!
       } else {
-        reject({ status: 404 });
+        reject({ status: 404 }); //reject with a value!
       }
-    }, 600);
+    }, 1000);
   });
 };
 
 fakeRequest("/users")
   .then((res) => {
-    console.log(`Status code:`, res.status);
-    console.log(`Data:`, res.data);
+    console.log(res);
+    const id = res.data[0].id;
+    return fakeRequest(`/users/${id}`);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("Request failed");
-  });
-
-fakeRequest("/dogs")
   .then((res) => {
-    console.log(`Status code:`, res.status);
-    console.log(`Data:`, res.data);
-    console.log("Request worked");
+    console.log(res);
+    const postId = res.data.topPostId;
+    return fakeRequest(`/posts/${postId}`);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("Request failed");
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log("OH NO!", err);
   });
